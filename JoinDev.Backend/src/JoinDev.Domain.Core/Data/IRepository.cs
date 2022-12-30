ca5fb6c7025fe98d947eeb63685f62d20472867a
Victor Cobo <v.cobof@outlook.com>
2022-12-30 09:27:29 -0300

    Data interfaces on domain

---
+++ JoinDev.Backend/src/JoinDev.Domain.Core/Data/IRepository.cs
@@ -0,0 +1,9 @@
+﻿using JoinDev.Domain.Core.DomainObjects;
+
+namespace JoinDev.Domain.Core.Data
+{
+    public interface IRepository<T> : IDisposable where T : IAggregateRoot
+    {
+        IUnitOfWork UnitOfWork { get; }
+    }
+}
