cd018d27da640c2e8088227a8ae7fd8cc9bf615b
Victor Cobo <v.cobof@outlook.com>
2022-12-31 19:37:55 -0300

    Link mapping

---
+++ JoinDev.Backend/src/JoinDev.Infra.Data/Mappings/LinkHierarchy/LinkMapping.cs
@@ -0,0 +1,16 @@
+﻿using JoinDev.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace JoinDev.Infra.Data.Mappings.LinkHierarchy
+{
+    public class LinkMapping : IEntityTypeConfiguration<Link>
+    {
+        public void Configure(EntityTypeBuilder<Link> builder)
+        {
+            builder.HasKey(c => c.Id);
+           
+            builder.ToTable("Links");
+        }
+    }
+}
