d4b6a3600d0b1f516926fa0a704ccd9fd4e03373
Victor Cobo <v.cobof@outlook.com>
2022-12-30 21:40:46 -0300

    unit of work, where the repositories will be centered

---
+++ JoinDev.Backend/src/JoinDev.Domain/Data/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using JoinDev.Domain.Core.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JoinDev.Domain.Data
+{
+    public interface IUnitOfWork : IUnitOfWorkBase
+    {
+    }
+}
